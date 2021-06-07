eb5976ea8e3fe1b3df40a021e619f1271cebfefc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-07 18:07:18 +0200

    bullhead: Set ro.vndk.version to current

--- device.mk
+++ device.mk
@@ -394,6 +394,9 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vndk.version=current
+
 # VTS {Library used for VTS profiling (only for userdebug and eng builds)}
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
