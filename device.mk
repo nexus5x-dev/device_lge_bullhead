a358a93b36aa618590d8bbd441a0dd9b7cf44149
hiru <rlawoehd187@naver.com>
2021-11-19 01:40:04 +0900

    Revert "Bullhead: Use new binderized light hal"
    This reverts commit 6f67efbbb66e4523433c45f11ae28134f64fa833.
    
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- device.mk
+++ device.mk
@@ -252,8 +252,10 @@ PRODUCT_COPY_FILES += \
 
 # Light HAL
 PRODUCT_PACKAGES += \
+    lights.bullhead \
     lights.vts \
-    android.hardware.light@2.0-service.bullhead
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # MBN
 PRODUCT_COPY_FILES += \
