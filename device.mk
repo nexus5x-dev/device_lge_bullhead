03bf4f5c12d2cf2eb6c4e27b13a2d7fe2bcbcad4
Luofan Chen <clfbbn@gmail.com>
2022-10-16 14:13:40 +0800

    bullhead: Build hidl.base shim from compat
    Change-Id: Idd8054ffedeb1e130c68d038e459ec51cb14e111

--- device.mk
+++ device.mk
@@ -175,7 +175,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey \
-    libshim_drm
+    libshim_drm \
+    android.hidl.base@1.0.vendor
 
 
 # Dumpstate HAL
