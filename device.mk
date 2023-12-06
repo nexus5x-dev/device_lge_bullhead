12cf175d28de20964aa8ffc0637b6f1a90543290
TARKZiM <tom8476oo@gmail.com>
2023-12-06 11:12:53 +0800

    bullhead: Remove duplicated vndk props

--- device.mk
+++ device.mk
@@ -413,9 +413,6 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vndk.version=current
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
