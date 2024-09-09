3e2516a95059714e9d2b4281b596341e360fd2d6
TARKZiM <tom8476oo@gmail.com>
2024-09-09 19:42:15 +0800

    bullhead: liblight: Move to vendor
    * For using libhardware_headers.

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -28,8 +28,10 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE := lights.bullhead
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
